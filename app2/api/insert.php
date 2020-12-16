d76e10c3f389a199e8eb31ddd2255e80856c74b3
mackliu <macklun@gmail.com>
2020-12-16 13:45:22 +0800

    先以純php 的方式製新增及讀出資料的動作

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,5 +1,9 @@
 <?php
 //處理新增資料的請求
+include_once "../base.php";
 
 
+$Stu->save($_POST);
+
+to("../index.php");
 ?>
--- app2/index.php
+++ app2/index.php
@@ -1,3 +1,4 @@
+<?php include_once "base.php";?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +8,54 @@
     <title>學生資料管理系統</title>
     <link rel="shortcut icon" href="#" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
+    <style>
+
+  table td{
+    padding:5px 10px;
+    border:0.5px solid #ccc;
+  }
+
+    </style>
     <!---匯入要使用的函式庫--->
 
 
 </head>
 <body>
   <h1 class="header">學生資料管理系統</h1>
-
+  <h5>新增學生</h5>
+  <form action="api/insert.php" method='POST'>
+    <div>姓名<input type="text" name='name'></div>
+    <div>班級<input type="text" name='classes'></div>
+    <div>座號<input type="text" name='num'></div>
+    <div>成績<input type="text" name='score'></div>
+    <input type="submit" value="新增">   
+  </form>
   
+<table>
+  <tr>
+    <td>id</td>
+    <td>name</td>
+    <td>classes</td>
+    <td>num</td>
+    <td>score</td>
+  </tr>
+  <?php
+  $rows=$Stu->all();
+
+  foreach($rows as $row){
+  ?>
+  <tr>
+    <td><?=$row['id'];?></td>
+    <td><?=$row['name'];?></td>
+    <td><?=$row['classes'];?></td>
+    <td><?=$row['num'];?></td>
+    <td><?=$row['score'];?></td>
+  </tr>
+  <?php
+}
+  ?>
+</table>
+
 </body>
 </html>
 <script>
