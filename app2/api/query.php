1f790d6c699a356485d3ab3297a62f5a37aa9d57
mackliu <macklun@gmail.com>
2020-12-16 15:04:23 +0800

    新增後即時在頁面更新學生列表內容

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,18 @@
 <?php
 //處理查詢資料的請求
+include_once "../base.php";
+
+$rows=$Stu->all();
+
+foreach($rows as $row){
+  echo  "<tr>";
+  echo  "  <td>{$row['id']}</td>";
+  echo  "  <td>{$row['name']}</td>";
+  echo  "  <td>{$row['classes']}</td>";
+  echo  "  <td>{$row['num']}</td>";
+  echo  "  <td>{$row['score']}</td>";
+  echo  "</tr> ";
+}
 
 
 ?>
